f10520404b34880e5ac744cbc14ecdbeefc6f003
SLAB48 <SLAB48@KM101-13-M>
2026-02-06 11:56:44 +0100

    Api fonctionnelle

---
+++ Pokemon/Services/GetPokemon.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pokemon.Services
+{
+    public class GetPokemon
+    {
+        public async Task<Root> GetApiPokemon(string poke)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://tyradex.app/api/v1/Pokemon/"+poke);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                Root pokeData = JsonConvert.DeserializeObject<Root>(jsonResponse);
+                try
+                {
+                    pokeData = JsonConvert.DeserializeObject<Root>(jsonResponse);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur JSON : " + ex.Message);
+                    return null;
+                }
+
+
+                return pokeData;
+            }
+
+            else
+            {
+                MessageBox.Show("Error fetching weather data.");
+                return null;
+            }
+        }
+    }
+}
