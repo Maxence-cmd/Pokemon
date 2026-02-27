cab33045bf112f7499b20f411c3a87aa33caac39
SLAB48 <SLAB48@KM101-13-M>
2026-02-27 11:24:55 +0100

    Yolo

--- Pokemon/MainWindow.xaml.cs
+++ Pokemon/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using Pokemon.Services;
 using Newtonsoft.Json;
+using Pokemon.Models;
 
 namespace Pokemon
 {
@@ -24,9 +25,11 @@ namespace Pokemon
     public partial class MainWindow : Window
     {
         public GetPokemon getPokemon;
+        public GetIDFromName getIDFromName;
         public MainWindow()
         {
             InitializeComponent();
+            getIDFromName = new GetIDFromName();
             getPokemon = new GetPokemon();
             string idPoke = "1";
         _: GetPokemon(idPoke);
@@ -69,7 +72,7 @@ namespace Pokemon
             Pre pre = asyncTask.pre;
             List<Resistance> resistances = asyncTask.resistances;
             NomPokemon.Text = name.fr;
-            NumPokemon.Text = "#"+root.pokedex_id.ToString();
+            NumPokemon.Text = "#" + root.pokedex_id.ToString();
             ImgPokemon.Source = new BitmapImage(new Uri(sprites.regular));
             Type1Pokemon.Source = new BitmapImage(new Uri(root.types[0].image));
             try 
@@ -81,11 +84,60 @@ namespace Pokemon
                 Type2Pokemon.Source = null;
             }
             PVPokemon.Value = stats.hp;
+            PVNumPokemon.Text = stats.hp.ToString();
             AttPokemon.Value = stats.atk;
+            AttNumPokemon.Text = stats.atk.ToString();
             DefPokemon.Value = stats.def;
+            DefNumPokemon.Text = stats.def.ToString();
             DefSpePokemon.Value = stats.spe_def;
+            DefSpeNumPokemon.Text = stats.spe_def.ToString();
             AttSpePokemon.Value = stats.spe_atk;
+            AttSpeNumPokemon.Text = stats.spe_atk.ToString();
             VitPokemon.Value = stats.vit;
+            VitNumPokemon.Text = stats.vit.ToString();
+        }
+
+        private async void BttRecherche_Click(object sender, RoutedEventArgs e)
+        {
+
+            string idPoke = NumPokeRecherche.Text.Trim().ToLower();
+
+
+            if (string.IsNullOrWhiteSpace(idPoke))
+            {
+                MessageBox.Show("Veuillez entrer un nom ou un numéro de Pokémon valide.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (int.TryParse(idPoke, out int numero))
+            {
+                if (numero < 1 || numero > 1025)
+                {
+                    MessageBox.Show("Veuillez entrer un numéro de Pokémon valide (entre 1 et 1025).", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                _: GetPokemon(numero.ToString());
+            }
+            else
+            {
+                try
+                {
+                _: GetPokemon(idPoke);
+                }
+                catch
+                {
+                    MessageBox.Show("Pokémon introuvable, veuillez vérifier le nom.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                //if ((string.IsNullOrWhiteSpace(idPoke)))
+                //{
+                //// On a trouvé le Pokémon, on appelle GetPokemon avec l'ID
+                //_: GetPokemon(idPoke);
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Pokémon introuvable, veuillez vérifier le nom.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                //}
+            }
         }
     }
 }
