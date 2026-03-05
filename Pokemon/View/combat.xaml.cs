c31a20d2e762463cf0d442b26ab8bfeac4d4d42e
SLAB47 <SLAB47@KM102-01-M>
2026-03-05 10:41:18 +0100

    dqsd

---
+++ Pokemon/View/combat.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pokemon.View
+{
+    /// <summary>
+    /// Logique d'interaction pour combat.xaml
+    /// </summary>
+    public partial class combat : UserControl
+    {
+        public combat()
+        {
+            InitializeComponent();
+        }
+
+        private void RandomPokemonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
